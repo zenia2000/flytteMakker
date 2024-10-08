7ad67e53fa8a47b07493b9a5efc2944aa27a4208
Zenia2000 <sarahenya2000@hotmail.dk>
2024-10-08 14:31:31 +0200

    Moved code from home.html and home.js into index and java. Added some styling and did some tidy up. Both home files were removed.

--- java.js
+++ java.js
@@ -1,11 +1,8 @@
-let text = "";
-let idCount = 0;
-
 const tasks =   [
-    {'dato' :'1','opg': 'a'},
-    {'dato' :'2','opg': 'b'},
-    {'dato': '3','opg': 'c'},
-    {'dato': '4','opg': 'd'}
+    {'id' : '1', 'link' :'<a href="https://www.borger.dk">Borger.dk</a>','opg': 'Flytte adresse på borger.dk', 'done' : '[_]' , 'missing' : '[_]'},
+    {'id' : '2', 'link' :'<a href="https://www.borger.dk">Borger.dk</a>','opg': 'Vælg Læge', 'done': '[_]' , 'missing' : '[_]'},
+    {'id' : '3', 'link': '<a href="https://www.silvan.dk/produkter/bolig-moebler/opbevaring/flyttekasser-tilbehoer">Silvan.dk</a>','opg': 'Køb flytte kasser', 'done': '[_]' , 'missing' : '[_]'},
+    {'id' : '4', 'link': ' ','opg': 'spørg forældre om hjælp til flyt af ting', 'done': '[_]' , 'missing':'[_]'}
             ]; //creates an array called tasks - with objects.
 
 buildTable(tasks)
@@ -15,56 +12,53 @@ function buildTable(data){
     let table = document.getElementById('myTable')
     for (let c=0;c<data.length;c++){
         let row = `<tr>
-            <td>${data[c].dato}</td>
+            <td>${data[c].id}</td>
+            <td>${data[c].link}</td>
             <td>${data[c].opg}</td>
+            <td>${data[c].done}</td>
+            <td>${data[c].missing}</td>
         </tr>`
 
         table.innerHTML += row
+        document.getElementById("myBtn").addEventListener("click",ryd);
+        document.getElementById("red").addEventListener("click",red);
+
 
     }
 }
 
-//Different attempt at adding new stuff
-function addNew(){
- let byId1    = document.getElementById('dateInput');
- let byId2    = document.getElementById('textInput');
-tasks.push(byId1,byId2);
+function ryd(){
+    document.getElementById("myTable").innerHTML = "";
 }
 
+function red(){
+    document.getElementById("myTable").style.color = "red"
+}
 
-//attempt at adding new tasks to table
-let btnAdd = document.querySelector('button');
-let dateInput = document.querySelector('input');
-let textInput = document.querySelector('input');
+// ----------------------------------------------------------------------------------------------------------------
+
+const buy = ["Havregryn","Pasta", "Ketchup"]
+
+function addToArray(){
+    let template = buy.map(item => `<li>${item}</li>`).join('\n');
+    document.querySelector('ul').innerHTML = template;
+}
+
+addToArray();
+
+let btnAdd = document.getElementById('add');
+let textInput = document.getElementById('input');
 
 btnAdd.addEventListener('click', function () {
-    tasks.push(dateInput.value , textInput.value);
-    buildTable(tasks);
+    buy.push(textInput.value);
+    textInput.value = '';
+    addToArray()
 });
 
 
+let btnDelete = document.getElementById('delete');
 
-/*
-//loops the array, each item on a new line. Gives each item an id.
-for (let x=0; x<tasks.length;x++){
-
-    idCount = tasks[x];
-    text += tasks[x] + " Id: " + idCount + "<br>";
-}
-
-//gives array an id useable by html.
-document.getElementById("taskList").innerHTML = text;
-
-document.getElementById("id").innerHTML = idCount;
-function removeFunc(x){
-    if (idCount == tasks[x]){
-        tasks.pop(x);
-    }
-}
-
-function showId(x){
-    if (idCount == tasks[x]){
-        return idCount;
-    }
-}
-*/
+btnDelete.addEventListener('click', function () {
+    buy.pop();
+    addToArray()
+});
